7c348a305f2f26b63a59b7157678cc2b7ff72c31
warrinerbros <nicholas.warriner@protonmail.com>
2022-02-11 18:11:20 -0500

    added project

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace ACH_Invalid_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check for correct amount of command line arguments
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Error: Expected one argument (the path of the file to check)");
+                return;
+            }
+
+            // Get file contents organized by line
+            string[] lines = System.IO.File.ReadAllLines(args[0]);
+
+            // String of all whitelisted characters
+            string alphameric = 
+            "abcdefghijklmnopqrstuvwxyz" +
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "0123456789" +
+            "_-:.@$=/ ";
+
+            // Loop for each line in the file
+            for (int i = 0; i < lines.Length; i++)
+            {
+                //Loop for each character in this line
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    // Alert the user if this is an illegal character
+                    if (!alphameric.Contains(lines[i][j]))
+                    {
+                        Console.WriteLine("Invalid character at line " + (i+1) + " position " + (j+1));
+                        Console.WriteLine(lines[i]);
+                        string indicator_str = new string(' ', lines[i].Length);
+                        StringBuilder indicator_sb = new StringBuilder(indicator_str);
+                        indicator_sb.Insert(j, '^');
+                        Console.WriteLine(indicator_sb);
+                        Console.WriteLine("");
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ACH_Find_Invalid_Characters
+
+This is a program created by Nick Warriner as part of the interview process with Magic-Wrighter. This project checks if an ACH file contains invalid characters and shows their locations.
+
+This project can be run with `dotnet run /location/of/file.ach`
+
+It can be ran with the included ACH file as `dotnet run invalid-chars.ach` from this project's directory.
